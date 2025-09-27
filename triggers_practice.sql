5b2b37b09946246982340f161ac47d9dd8d71d0f
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-09-27 21:02:12 +0530

    triggers

--- triggers_practice.sql
+++ triggers_practice.sql
@@ -5,88 +5,7 @@ select * from
 (select productid,price,avg(price) over() avgprice from salesdb.products) as f where price>avgprice;
 
 
-drop table messages;
-create table message(message varchar(225));
- 
- drop trigger trigger_emp_msg;
- drop trigger employee_insert_trigger;
- 
- 
- 
- /*Triggers */
- -- Creating a Trigger on employee on delete action
- delimiter ##
- create trigger employee_delete_trigger
- after delete on employee for each row
- begin
- insert into message values(concat("employee ",old.emp_name,"  is deleted"));
- end ##
- delimiter ;
- 
-DELETE FROM employee WHERE id = 8;
-SET SQL_SAFE_UPDATES = 0;
-DELETE FROM employee WHERE emp_name="srinu";
-SET SQL_SAFE_UPDATES = 1;
-
-
-
-
-
-use practice;
-create table products(products varchar(30),quantity int);
-create table orders(ord_product varchar(30),ord_quantity int);
-insert into products values("mobiles",20),("watches",10),("laptops",5);
-
-/* creating trigger on orders and products tables*/
-drop trigger sell_products;
-delimiter //
-CREATE TRIGGER SELL_PRODUCTS
-BEFORE INSERT ON ORDERS FOR each row
-BEGIN
-SET @available=0;
-select quantity into @available from products where products=new.ord_product;
-if @available>new.ord_quantity then
-update products set quantity=quantity-new.ord_quantity where products=new.ord_product;
-elseif @available=new.ord_quantity then
-update products set quantity=0 where products=new.ord_product;
-
-else
-signal sqlstate "45000"
-set message_text="available quantity is less than ordered quantity!!";
-end if;
-end //
-delimiter ;
-SET SQL_SAFE_UPDATES = 0;
-insert into orders values("laptops",5);
-SET SQL_SAFE_UPDATES = 1;
-show triggers;
-/* 
-CREATED TRIGGER ON EMP SALARY UPDATES
-
-*/
-use practice;
-DROP TABLE SALARY_LOGS;
-CREATE TABLE EMP_SALARY_LOGS(EMP_ID INT,MESSAGE VARCHAR(30));
-delimiter //
-CREATE TRIGGER after_sal_update
-AFTER UPDATE ON EMP FOR EACH ROW
-BEGIN
-IF NEW.SALARY>OLD.SALARY THEN
-   INSERT INTO EMP_SALARY_LOGS(EMP_ID,MESSAGE)
-   VALUES (NEW.EMP_NO,CONCAT(NEW.ENAME,'GOT A SALARY HIKE!'));
-elseif NEW.SALARY<OLD.SALARY THEN
-   INSERT INTO SALARY_LOGS(EMP_ID,MESSAGE)
-   VALUES(NEW.EMP_NO,concat(NEW.ENAME,'GOT A SALARY CUT !'));
-else
-  INSERT INTO SALARY_LOGS(EMP_ID,MESSAGE)
-  VALUES (NEW.EMP_NO,CONCAT(NEW.ENAME,'SALARY UNCHANGED !'));
-END IF;
-END //
-DELIMITER ;
-
-UPDATE EMP SET SALARY =2000 WHERE EMP_NO=7934;
-  
-
+/*TRIGGERS*/
 
 /* ============================================
    TRIGGER 1: Log Employee Deletions
@@ -105,8 +24,7 @@ DELIMITER ;
 
 /* Test the Trigger */
 SET SQL_SAFE_UPDATES = 0;
-DELETE FROM employee WHERE id = 8;
-DELETE FROM employee WHERE emp_name = "srinu";
+DELETE FROM employee WHERE id = 7;
 SET SQL_SAFE_UPDATES = 1;
 
 
